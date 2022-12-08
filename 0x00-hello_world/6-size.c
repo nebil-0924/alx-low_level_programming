beb782c3fbfe4e0bf99413a80457d6d3bfcd7c6d
nebil-0924 <nebilrahmeto8@gmail.com>
2022-12-08 07:54:47 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
 /**
  * main - print the size of various types
  *
- * description: print the size of various type on the computer
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,7 +16,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	
-	return (0);
 
+	return (0);
 }
