b7f4de337d2298a9f720b565ebaf735f740dceb6
nebil-0924 <nebilrahmeto8@gmail.com>
2022-12-22 10:36:51 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,28 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-* leet - function that encode a string
-*@str:string that will be encoded
-*Return:returns encoded string
-*/
+ * leet - function that encodes a string into 1337
+ * @s: string array
+ * Return: string array
+ */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i;
 
-	while (str[++index1])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
-		{
-			if (str[index1] == leet[index2] ||
-			 str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
-		}
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
 	}
-	return (str);
+
+	return (s);
 }
