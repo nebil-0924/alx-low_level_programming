ae5a5f6326497d5e90abb1963e1df2191a663add
nebil-0924 <nebilrahmeto8@gmail.com>
2023-01-13 05:24:46 -0800

    most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,11 @@ void print_most_numbers(void)
 	int num = 48;
 	for (num = 48; num <58; num++)
 	{
-		if ((num != 50) && (num != 52))
-			_putchar(num);
+		if ((num = 50) || (num = 52))
+		{
+			continue;
+		}
+		_putchar(num);
 	}
 	_putchar(10);
 }
