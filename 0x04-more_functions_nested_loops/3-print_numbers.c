1570690cf9fb93da19a9275178a1e991c5210524
nebil-0924 <nebilrahmeto8@gmail.com>
2023-01-13 04:51:45 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - pritn the numbers from 0 to 9
+ *
+ * Return: returns nothing
+ */
+void print_number(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		_putchar(a);
+		_putchar('\n');
+		a++;
+	}
+}
+
