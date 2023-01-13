18796a09289f63af9c916d89310736bb60054ea3
nebil-0924 <nebilrahmeto8@gmail.com>
2023-01-13 05:30:46 -0800

    most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_numbers - function that print all numbers from zero to nine except two and four
+ *print_most_numbers - function that print all numbers from zero to nine except two and four
  *
  *Return: returns nothing
 */
@@ -9,11 +9,8 @@ void print_most_numbers(void)
 	int num = 48;
 	for (num = 48; num < 58; num++)
 	{
-		if ((num = 50) && (num = 52))
-		{
-			continue;
-		}
-		_putchar(num);
+		if ((num != 50) && (num != 52))
+			_putchar(num);
 	}
 	_putchar(10);
 }
