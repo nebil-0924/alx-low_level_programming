9035042d74cdd9415de09d2da56ed2f224e5e6ca
nebil-0924 <nebilrahmeto8@gmail.com>
2023-01-13 06:16:21 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - print a diagonal line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_diagonal(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
